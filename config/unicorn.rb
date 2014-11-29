5f6dac1b6d14a6685fc46e42201d1f858bc647a1
Robert Fletcher <lobatifricha@gmail.com>
2014-11-28 18:36:19 -0800

    remove rails 4 database config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,10 +18,7 @@ after_fork do |_server, _worker|
   end
 
   if defined?(ActiveRecord::Base)
-    config = ActiveRecord::Base.configurations[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-    config['pool'] = ENV['DB_POOL'] || 7
-    ActiveRecord::Base.establish_connection(config)
+    ActiveRecord::Base.establish_connection
   end
 
 end
